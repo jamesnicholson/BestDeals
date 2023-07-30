adcc4d52cd46bcaee686d16b2d2151374fbf26ce
James Nicholson <jamesnicholson@Jamess-MacBook-Pro.local>
2023-07-30 22:24:29 +0200

    fixed image

--- src/components/molecules/Card/index.tsx
+++ src/components/molecules/Card/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Linking } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Product } from '../../../types';
 import Image from '../../atoms/Image';
 import Title from '../../atoms/Title';
@@ -9,11 +10,13 @@ interface CardProp {
 }
 const Card: React.FC<CardProp> = ({ product }) => {
   return (
+    <TouchableOpacity onPress={async () => await Linking.openURL(product.url)}>
     <View style={styles.card}>
-      <Image source={{ uri: product.img }} />
+      <Image source={{ uri: product.img }}  style={{ flex:1, width: "auto", height: 350}} />
       <Title>{product.title}</Title>
       <Title>{product.price.toString()}</Title>
     </View>
+    </TouchableOpacity>
   );
 };
 
